e97eaac35898ff0dab0e6061a02b8d9ab9957ab4
mrprocs <mrprocs@gmail.com>
2021-06-20 21:49:36 +0430

    Create instagram auto registeration app

---
+++ django_social_bot_instagram_register/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangoSocialBotInstagramRegisterConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_social_bot_instagram_register'
